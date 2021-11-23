181d03dff00efe3e657ca39feb2ecb6b4fc8fb13
Pedro Ramos <medpsr9@gmail.com>
2021-11-23 12:27:30 -0600

    commit tree ccb50576c0160ba75b2188dae0856d2e003d8fe7

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const feet = 264;
+
+function distanceFromHqInBlocks(block){
+    if (block >42){
+        return block - hq;
+    }else if (block < 42){
+            return hq- block;
+        }
+    }
+function distanceFromHqInFeet(block){
+return distanceFromHqInBlock(block) * feet
+}
+
+function distanceTravelledInFeet(start, destination){
+    let walkDistance= math.abs(destinastion - start)
+    return walkDistance * feet
+}
+function calculatesFarePrice (start, destinastion){
+    let n =distanceTravelledInFeet (start, destinastion)
+    if (n<=400) {
+        return 0;
+    }else if (n>400&& n<=2000){
+        return (n-400)*.02;
+    }else if (2500>n && n>2000){
+        return 25;
+    }else if(n>2500){
+        return 'cannot travel that far'
+    }
+}
